7539fdb2a2e99e4111ca0ffa0cc9bb32cc18e1e4
weru <onewesh@gmail.com>
2020-04-21 18:32:51 +0300

    move helper functions from main script

---
+++ assets/js/functions.js
@@ -0,0 +1,122 @@
+function isObj(obj) {
+  return (obj && typeof obj === 'object' && obj !== null) ? true : false;
+}
+
+function createEl(element = 'div') {
+  return document.createElement(element);
+}
+
+function elem(selector, parent = document){
+  let elem = parent.querySelector(selector);
+  return elem != false ? elem : false;
+}
+
+function elems(selector, parent = document) {
+  let elems = parent.querySelectorAll(selector);
+  return elems.length ? elems : false;
+}
+
+function pushClass(el, targetClass) {
+  if (isObj(el) && targetClass) {
+    elClass = el.classList;
+    elClass.contains(targetClass) ? false : elClass.add(targetClass);
+  }
+}
+
+function hasClasses(el) {
+  if(isObj(el)) {
+    const classes = el.classList;
+    return classes.length
+  }
+}
+
+(function markInlineCodeTags(){
+  const codeBlocks = elems('code');
+  if(codeBlocks) {
+    codeBlocks.forEach(function(codeBlock){
+      hasClasses(codeBlock) ? false: pushClass(codeBlock, 'noClass');
+    });
+  }
+})();
+
+function deleteClass(el, targetClass) {
+  if (isObj(el) && targetClass) {
+    elClass = el.classList;
+    elClass.contains(targetClass) ? elClass.remove(targetClass) : false;
+  }
+}
+
+function modifyClass(el, targetClass) {
+  if (isObj(el) && targetClass) {
+    elClass = el.classList;
+    elClass.contains(targetClass) ? elClass.remove(targetClass) : elClass.add(targetClass);
+  }
+}
+
+function containsClass(el, targetClass) {
+  if (isObj(el) && targetClass && el !== document ) {
+    return el.classList.contains(targetClass) ? true : false;
+  }
+}
+
+function elemAttribute(elem, attr, value = null) {
+  if (value) {
+    elem.setAttribute(attr, value);
+  } else {
+    value = elem.getAttribute(attr);
+    return value ? value : false;
+  }
+}
+
+function wrapEl(el, wrapper) {
+  el.parentNode.insertBefore(wrapper, el);
+  wrapper.appendChild(el);
+}
+
+function deleteChars(str, subs) {
+  let newStr = str;
+  if (Array.isArray(subs)) {
+    for (let i = 0; i < subs.length; i++) {
+      newStr = newStr.replace(subs[i], '');
+    }
+  } else {
+    newStr = newStr.replace(subs, '');
+  }
+  return newStr;
+}
+
+function isBlank(str) {
+  return (!str || str.trim().length === 0);
+}
+
+function isMatch(element, selectors) {
+  if(isObj(element)) {
+    if(selectors.isArray) {
+      let matching = selectors.map(function(selector){
+        return element.matches(selector)
+      })
+      return matching.includes(true);
+    }
+    return element.matches(selectors)
+  }
+}
+
+const copyToClipboard = str => {
+  let copy, selection, selected;
+  copy = createEl('textarea');
+  copy.value = str;
+  copy.setAttribute('readonly', '');
+  copy.style.position = 'absolute';
+  copy.style.left = '-9999px';
+  selection = document.getSelection();
+  doc.appendChild(copy);
+  // check if there is any selected content
+  selected = selection.rangeCount > 0 ? selection.getRangeAt(0) : false;
+  copy.select();
+  document.execCommand('copy');
+  doc.removeChild(copy);
+  if (selected) { // if a selection existed before copying
+    selection.removeAllRanges(); // unselect existing selection
+    selection.addRange(selected); // restore the original selection
+  }
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -5,109 +5,6 @@ function fileClosure(){
   const parentURL = '{{ absURL "" }}';
   const doc = document.documentElement;
   
-  function isObj(obj) {
-    return (obj && typeof obj === 'object' && obj !== null) ? true : false;
-  }
-  
-  function createEl(element = 'div') {
-    return document.createElement(element);
-  }
-  
-  function elem(selector, parent = document){
-    let elem = parent.querySelector(selector);
-    return elem != false ? elem : false;
-  }
-  
-  function elems(selector, parent = document) {
-    let elems = parent.querySelectorAll(selector);
-    return elems.length ? elems : false;
-  }
-  
-  function pushClass(el, targetClass) {
-    if (isObj(el) && targetClass) {
-      elClass = el.classList;
-      elClass.contains(targetClass) ? false : elClass.add(targetClass);
-    }
-  }
-  
-  function hasClasses(el) {
-    if(isObj(el)) {
-      const classes = el.classList;
-      return classes.length
-    }
-  }
-  
-  (function markInlineCodeTags(){
-    const codeBlocks = elems('code');
-    if(codeBlocks) {
-      codeBlocks.forEach(function(codeBlock){
-        hasClasses(codeBlock) ? false: pushClass(codeBlock, 'noClass');
-      });
-    }
-  })();
-  
-  function deleteClass(el, targetClass) {
-    if (isObj(el) && targetClass) {
-      elClass = el.classList;
-      elClass.contains(targetClass) ? elClass.remove(targetClass) : false;
-    }
-  }
-  
-  function modifyClass(el, targetClass) {
-    if (isObj(el) && targetClass) {
-      elClass = el.classList;
-      elClass.contains(targetClass) ? elClass.remove(targetClass) : elClass.add(targetClass);
-    }
-  }
-  
-  function containsClass(el, targetClass) {
-    if (isObj(el) && targetClass && el !== document ) {
-      return el.classList.contains(targetClass) ? true : false;
-    }
-  }
-  
-  function elemAttribute(elem, attr, value = null) {
-    if (value) {
-      elem.setAttribute(attr, value);
-    } else {
-      value = elem.getAttribute(attr);
-      return value ? value : false;
-    }
-  }
-  
-  function wrapEl(el, wrapper) {
-    el.parentNode.insertBefore(wrapper, el);
-    wrapper.appendChild(el);
-  }
-  
-  function deleteChars(str, subs) {
-    let newStr = str;
-    if (Array.isArray(subs)) {
-      for (let i = 0; i < subs.length; i++) {
-        newStr = newStr.replace(subs[i], '');
-      }
-    } else {
-      newStr = newStr.replace(subs, '');
-    }
-    return newStr;
-  }
-  
-  function isBlank(str) {
-    return (!str || str.trim().length === 0);
-  }
-  
-  function isMatch(element, selectors) {
-    if(isObj(element)) {
-      if(selectors.isArray) {
-        let matching = selectors.map(function(selector){
-          return element.matches(selector)
-        })
-        return matching.includes(true);
-      }
-      return element.matches(selectors)
-    }
-  }
-  
   (function updateDate() {
     var date = new Date();
     var year = date.getFullYear();
@@ -117,24 +14,6 @@ function fileClosure(){
   (function() {
     
   })();
-  
-  function convertToUnderScoreCase(str) {
-    let char, newChar, newStr;
-    newStr = '';
-    if (typeof str == 'string') {
-      for (let x = 0; x < str.length; x++) {
-        char = str.charAt(x);
-        if (char.match(/^[A-Z]*$/)) {
-          char = char.toLowerCase();
-          newChar = `_${char}`
-          newStr += newChar;
-        } else {
-          newStr += char;
-        }
-      }
-      return newStr;
-    }
-  }
   
   (function makeExternalLinks(){
     let links = elems('a');
@@ -191,26 +70,6 @@ function fileClosure(){
     })
   }
   
-  const copyToClipboard = str => {
-    let copy, selection, selected;
-    copy = createEl('textarea');
-    copy.value = str;
-    copy.setAttribute('readonly', '');
-    copy.style.position = 'absolute';
-    copy.style.left = '-9999px';
-    selection = document.getSelection();
-    doc.appendChild(copy);
-    // check if there is any selected content
-    selected = selection.rangeCount > 0 ? selection.getRangeAt(0) : false;
-    copy.select();
-    document.execCommand('copy');
-    doc.removeChild(copy);
-    if (selected) { // if a selection existed before copying
-      selection.removeAllRanges(); // unselect existing selection
-      selection.addRange(selected); // restore the original selection
-    }
-  }
-  
   (function copyCode(){
     const markedCodeBlocks = elems('code');
     const copyClass = 'hljs-copy';
@@ -255,9 +114,7 @@ function fileClosure(){
         target.textContent = copyFeedbackLabel;
       }
     })
-    
   })();  
-  
   
   function copyFeedback(parent) {
     const copyText = document.createElement('div');
@@ -350,8 +207,6 @@ function fileClosure(){
       });
     }
   })();
-  
-  // add new code above this line
   
   function populateAlt(images) {
     images.forEach((image) => {
@@ -549,6 +404,7 @@ function fileClosure(){
       }
     });
   })();
-  
+
+  // add new code above this line
 }
 window.addEventListener('load', fileClosure());

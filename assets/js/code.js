edd4b3530130c01b48896615865242daf5990270
weru <onewesh@gmail.com>
2020-05-18 20:42:43 +0300

    add expand function

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,20 +1,110 @@
+/*
+
+1. show action buttons conditionally
+2. don't show codewrap icon if it's not needed
+3. don't show a code expand icon if it's not needed
+
+4. detect line width& height changes
+
+
+*/
+
 const codeActionButtons = [
   {
     icon: 'copy', 
     id: 'copy',
-    title: 'Copy Code'
+    title: 'Copy Code',
+    show: true
   },
   {
     icon: 'order',
     id: 'lines',
-    title: 'Toggle Line Numbers' 
+    title: 'Toggle Line Numbers',
+    show: true 
   },
   {
     icon: 'carly',
     id: 'wrap',
-    title: 'Toggle Line Wrap' 
-  }
+    title: 'Toggle Line Wrap',
+    show: false
+  },
+  {
+    icon: 'expand',
+    id: 'expand',
+    title: 'Expand code block',
+    show: false 
+  },
+  // {
+  //   icon: 'light',
+  //   id: 'light',
+  //   title: 'Change code block theme',
+  //   show: false 
+  // }
 ];
+
+function codeBlocks() {
+  const markedCodeBlocks = elems('code');
+  const blocks = Array.from(markedCodeBlocks).filter(function(block){
+    return hasClasses(block) && !Array.from(block.classList).includes('noClass');
+  }).map(function(block){
+    return block
+  });
+  return blocks;
+}
+
+function codeBlockFits(block) {
+  // return false if codeblock overflows
+  const blockWidth = block.offsetWidth;
+  const highlightBlockWidth = block.parentNode.parentNode.offsetWidth;
+  return blockWidth <= highlightBlockWidth ? true : false;
+}
+
+function maxHeightIsSet(elem) {
+  let maxHeight = elem.style.maxHeight;
+  return maxHeight.includes('px')
+}
+
+function expandCodeBlock() {
+  const blocks = codeBlocks();
+  const body = elem('body');
+  const maxLines = parseInt(body.dataset.code);
+
+  const tallBlocks = [];
+  
+  Array.from(blocks).map(function(block){
+    const lines = elems('.ln', block);
+    const codeLines = lines.length;
+    if (codeLines > maxLines) {
+      tallBlocks.push(lines);
+    }
+  });
+  
+  tallBlocks.forEach(function(lines){
+
+    const lastLine = lines[maxLines-1];
+
+    const maxCodeBlockHeight = lastLine.offsetTop;
+
+    const codeBlock = lines[0].parentNode;
+
+    codeBlock.style.maxHeight = maxHeightIsSet(codeBlock) ? `100vh` : `${maxCodeBlockHeight}px`;
+
+    const highlightElement = codeBlock.parentNode.parentNode;
+    
+    // wait for a second before quering the expand button
+    setTimeout(function(){
+      const expandIcon = elem('.panel_expand', highlightElement.nextElementSibling);
+      deleteClass(expandIcon, 'panel_hide');
+    }, 1000)
+
+  });
+}
+
+expandCodeBlock();
+
+window.addEventListener('resize', function(event) {
+  expandCodeBlock();
+});
 
 function actionPanel() {
   const panel = createEl();
@@ -26,12 +116,13 @@ function actionPanel() {
     btn.href = '#';
     btn.title = button.title;
     btn.className = `icon panel_icon panel_${button.id}`;
+    button.show ? false : pushClass(btn, 'panel_hide');
     // load icon inside button
     loadSvg(button.icon, btn);
     // append button on panel
     panel.appendChild(btn);
   });
-
+  
   return panel;
 }
 
@@ -41,6 +132,7 @@ function toggleLineNumbers(elems) {
 
 function toggleLineWrap(elem) {
   modifyClass(elem, 'pre_wrap');
+  // expandCodeBlock();
 }
 
 function copyCode(codeElement) {
@@ -55,17 +147,12 @@ function copyCode(codeElement) {
   
   const codeToCopy = codeElement.textContent;
   // copy code
-
+  
   copyToClipboard(codeToCopy);
 }
 
 (function codeActions(){
-  const markedCodeBlocks = elems('code');
-  const blocks = Array.from(markedCodeBlocks).filter(function(block){
-    return hasClasses(block) && !Array.from(block.classList).includes('noClass');
-  }).map(function(block){
-    return block
-  });
+  const blocks = codeBlocks();
   
   const highlightWrapId = 'highlight_wrap';
   blocks.forEach(function(block){
@@ -74,8 +161,12 @@ function copyCode(codeElement) {
     const highlightWrapper = createEl();
     highlightWrapper.className = highlightWrapId;
     wrapEl(highlightElement, highlightWrapper);
-
+    
     const panel = actionPanel();
+    // show wrap icon only if the code block needs wrapping
+    const wrapIcon = elem('.panel_wrap', panel);
+    codeBlockFits(block) ? false : deleteClass(wrapIcon, 'panel_hide');
+
     // append buttons 
     highlightWrapper.appendChild(panel);
   });
@@ -83,12 +174,13 @@ function copyCode(codeElement) {
   const copyId = 'panel_copy';
   const wrapId = 'panel_wrap';
   const linesId = 'panel_lines';
-
+  const expandId = 'panel_expand';
+  
   function isItem(target, id) {
     // if is item or within item
     return target.matches(`.${id}`) || target.closest(`.${id}`);
   }
-
+  
   function showActive(target, targetClass,activeClass = 'active') {
     const active = activeClass;
     const targetElement = target.matches(`.${targetClass}`) ? target : target.closest(`.${targetClass}`);
@@ -100,31 +192,34 @@ function copyCode(codeElement) {
     });
     modifyClass(targetElement, active);
   }
-
+  
   doc.addEventListener('click', function(event){
     // copy code block
     const target = event.target;
     const isCopyIcon = isItem(target, copyId);
     const isWrapIcon = isItem(target, wrapId);
     const isLinesIcon = isItem(target, linesId);
-    const isActionable = isCopyIcon || isWrapIcon || isLinesIcon;
+    const isExpandIcon = isItem(target, expandId);
+    const isActionable = isCopyIcon || isWrapIcon || isLinesIcon || isExpandIcon;
     
     if(isActionable) {
       event.preventDefault();
       showActive(target, 'icon');
       const codeElement = target.closest(`.${highlightWrapId}`).firstElementChild.firstElementChild;
       let lineNumbers = elems('.ln', codeElement);
-
+      
       isWrapIcon ? toggleLineWrap(codeElement) : false; 
-
+      
       isLinesIcon ? toggleLineNumbers(lineNumbers) : false;
 
+      isExpandIcon ? expandCodeBlock() : false;
+      
       if(isCopyIcon) {
         // clone code element
         const codeElementClone = codeElement.cloneNode(true);
         copyCode(codeElementClone);
       }
     }
+  });
   
-  })
 })();  

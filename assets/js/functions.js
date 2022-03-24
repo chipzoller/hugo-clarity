7a6ea9fc7695d77470716e00fd5f9800c84db2da
weru <fromweru@gmail.com>
2022-03-24 20:50:47 +0300

    feat: add search function #49
    Signed-off-by: weru <fromweru@gmail.com>

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -183,6 +183,64 @@ function parseBoolean(string) {
   }
 };
 
+function wrapText(text, context, wrapper = 'mark') {
+  let open = `<${wrapper}>`;
+  let close = `</${wrapper}>`;
+  let escapedOpen = `%3C${wrapper}%3E`;
+  let escapedClose = `%3C/${wrapper}%3E`;
+  function wrap(context) {
+    let c = context.innerHTML;
+    let pattern = new RegExp(text, "gi");
+    let matches = text.length ? c.match(pattern) : null;
+
+    if(matches) {
+      matches.forEach(function(matchStr){
+        c = c.replaceAll(matchStr, `${open}${matchStr}${close}`);
+        context.innerHTML = c;
+      });
+
+      const images = elems('img', context);
+
+      if(images) {
+        images.forEach(image => {
+          image.src = image.src.replaceAll(open, '').replaceAll(close, '').replaceAll(escapedOpen, '').replaceAll(escapedClose, '');
+        });
+      }
+    }
+  }
+
+  const contents = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "code", "td"];
+
+  contents.forEach(function(c){
+    const cs = elems(c, context);
+    if(cs.length) {
+      cs.forEach(function(cx, index){
+        if(cx.children.length >= 1) {
+          Array.from(cx.children).forEach(function(child){
+            wrap(child);
+          })
+          wrap(cx);
+        } else {
+          wrap(cx);
+        }
+        // sanitize urls and ids
+      });
+    }
+  });
+
+  const hyperLinks = elems('a');
+  if(hyperLinks) {
+    hyperLinks.forEach(function(link){
+      link.href = link.href.replaceAll(encodeURI(open), "").replaceAll(encodeURI(close), "");
+    });
+  }
+}
+
+function emptyEl(el) {
+  while(el.firstChild)
+  el.removeChild(el.firstChild);
+}
+
 (function() {
   const bodyElement = elem('body');
   const platform = navigator.platform.toLowerCase();
